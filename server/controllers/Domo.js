9bacfd439170d30d73035145f7632a71cc96eef9
Kristin <kmyap128@gmail.com>
2025-04-16 12:51:47 -0400

    domo lists implemented

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -3,15 +3,7 @@ const models = require('../models');
 const { Domo } = models;
 
 const makerPage = async (req, res) => {
-  try {
-    const query = { owner: req.session.account._id };
-    const docs = await Domo.find(query).select('name age').lean().exec();
-
-    return res.render('app', { domos: docs });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Error retrieving domos!' });
-  }
+  return res.render('app');
 };
 
 const makeDomo = async (req, res) => {
@@ -38,7 +30,20 @@ const makeDomo = async (req, res) => {
   }
 };
 
+const getDomos = async (req, res) => {
+  try {
+    const query = {owner: req.session.account._id};
+    const docs = await Domo.find(query).select('name age').lean().exec();
+
+    return res.json({domos: docs});
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'Error retrieving domos!'});
+  }
+};
+
 module.exports = {
   makerPage,
   makeDomo,
+  getDomos,
 };
